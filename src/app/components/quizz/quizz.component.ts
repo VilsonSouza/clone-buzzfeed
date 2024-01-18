4670ada377f277c980ed41ff171cbf59d040f43b
VilsonSouza <vilsonsouza758@gmail.com>
2024-01-18 11:01:13 -0300

    all items

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -10,18 +10,16 @@ import quizz_questions from "../../../assets/data/quizz_questions.json"
 export class QuizzComponent implements OnInit {
 
   title:string = ""
-
   questions:any
   questionSelected:any
 
   answers:string[] = []
-  answerSelected:string =""
+  answerSelected:string = ""
 
-  questionIndex:number =0
-  questionMaxIndex:number=0
+  questionIndex: number = 0
+  questionMaxIndex:number = 0
 
-  finished:boolean = false
-
+  finished: boolean = false
   constructor() { }
 
   ngOnInit(): void {
@@ -31,45 +29,35 @@ export class QuizzComponent implements OnInit {
 
       this.questions = quizz_questions.questions
       this.questionSelected = this.questions[this.questionIndex]
-
       this.questionIndex = 0
       this.questionMaxIndex = this.questions.length
 
-      console.log(this.questionIndex)
-      console.log(this.questionMaxIndex)
     }
-
   }
-
   playerChoose(value:string){
     this.answers.push(value)
     this.nextStep()
-
   }
-
   async nextStep(){
-    this.questionIndex+=1
+    this.questionIndex++
 
     if(this.questionMaxIndex > this.questionIndex){
-        this.questionSelected = this.questions[this.questionIndex]
+      this.questionSelected = this.questions[this.questionIndex]
     }else{
       const finalAnswer:string = await this.checkResult(this.answers)
       this.finished = true
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+
     }
+
   }
-
-  async checkResult(anwsers:string[]){
-
-    const result = anwsers.reduce((previous, current, i, arr)=>{
-        if(
-          arr.filter(item => item === previous).length >
-          arr.filter(item => item === current).length
-        ){
-          return previous
-        }else{
-          return current
-        }
+  async checkResult(answers: string[]){
+    const result = answers.reduce((previous,current,i,arr)=>{
+      if(arr.filter(item => item === previous).length> arr.filter(item => item === current).length){
+        return previous
+      }else{
+        return current
+      }
     })
 
     return result
